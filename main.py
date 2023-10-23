c33dee2353238757747affda5a85aff947bef5a3
Sidd <sidd.s.pai@gmail.com>
2023-10-22 20:08:22 -0600

    Extracted channel generation and cleaned up code.

--- main.py
+++ main.py
@@ -4,8 +4,11 @@ import random
 import statistics
 import string
 import time
+from functools import partial
+from itertools import filterfalse
 from operator import countOf
 from pprint import pprint
+from typing import Iterable
 
 from graph import DEFAULT_TRANSACTION_VALUE, Graph
 from utils import TxData, TxStatus
@@ -18,49 +21,51 @@ def remaining_nodes(graph: Graph, node: str) -> list[str]:
     return list(graph.keys() - graph[node].keys() - {node})
 
 
-def main() -> None:
-    graph = Graph(string.ascii_lowercase)
+def max_channels_created(graph: Graph, node: str) -> bool:
+    return len(graph[node]) == CHANNELS_PER_NODE
 
-    for node in graph.nodes:
-        if len(graph[node]) == CHANNELS_PER_NODE:
-            continue
-        for neighbour in remaining_nodes(graph, node):
-            if len(graph[neighbour]) == CHANNELS_PER_NODE:
-                continue
+
+def unpopulated_nodes(graph: Graph, nodes: Iterable[str]) -> Iterable[str]:
+    return filterfalse(partial(max_channels_created, graph), nodes)
+
+
+def generate_channels(graph: Graph) -> None:
+    nodes = unpopulated_nodes(graph, graph.nodes)
+    for node in nodes:
+        neighbours = unpopulated_nodes(graph, remaining_nodes(graph, node))
+        for neighbour in neighbours:
             graph.open_channel(node, neighbour)
-            if len(graph[node]) == CHANNELS_PER_NODE:
+            if max_channels_created(graph, node):
                 break
 
-    # Keep track of the number of successful transactions,
-    # failed transactions, and the number of transactions
-    # attempted.
-    i = 0
-    t0 = time.perf_counter()
-    txs: list[TxData] = []
-    while i < NUMBER_OF_TRANSACTIONS:
-        # Randomly select two nodes from the network to transact.
-        sender, receiver = random.sample(graph.nodes, 2)
 
-        # If the node itself does not have the proper funds to send
-        # a transaction, we skip this loop iteration since this is
-        # not a failure on the network to route a payment.
+def main() -> None:
+    graph = Graph(string.ascii_lowercase)
+    # Populate graph with channels.
+    generate_channels(graph)
+    # Track our attempted transactions.
+    attempts = 0
+    txs: list[TxData] = []
+    t0 = time.perf_counter()
+    while attempts < NUMBER_OF_TRANSACTIONS:
+        # Randomly select two nodes from the network.
+        sender, receiver = random.sample(graph.nodes, 2)
         if graph.get_balance(sender) < DEFAULT_TRANSACTION_VALUE:
             continue
-
         txs.append(graph.send(sender, receiver))
-        i += 1
+        attempts += 1
     t1 = time.perf_counter()
     successes = countOf((tx.status for tx in txs), TxStatus.SUCCESS)
-    failures = i - successes
+    failures = attempts - successes
     avg_hops = statistics.mean(tx.hops for tx in txs if tx.status == TxStatus.SUCCESS)
     print(
         "=" * 80,
-        f"Attempted Transactions: {i:_}",
+        f"Attempted Transactions: {attempts:_}",
         f"Successes: {successes}",
         f"Failures: {failures}",
-        f"Success Rate: {successes/i:.2%}",
-        f"Attempted Transactions Per Second: {i/(t1-t0):.4f}",
-        f"Hops Per Successful Transaction: {avg_hops}",
+        f"Success Rate: {successes/attempts:.2%}",
+        f"Attempted Transactions Per Second: {attempts/(t1-t0):.4f}",
+        f"Average Hops Per Successful Transaction: {avg_hops:.1f}",
         "=" * 80,
         sep="\n",
     )
