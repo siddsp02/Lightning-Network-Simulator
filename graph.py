cdcee3118d01ec8b3ba5fc4ad08298dc60952a84
Sidd <sidd.s.pai@gmail.com>
2023-10-25 16:58:18 -0600

    Updated typehints and added `_from_dict` method.

--- graph.py
+++ graph.py
@@ -12,7 +12,10 @@ from utils import TxData, TxStatus
 BITCOIN_PRICE = 30_000
 SATOSHIS_PER_BITCOIN = 100_000_000
 
-INFINITY = UINT64_MAX = 2**64 - 1  # Consider as "infinity" or our maximum edge cost.
+# Consider as "infinity" or our maximum edge cost.
+# Any edge with a cost greater than or equal to
+# this value is unreachable in a graph.
+INFINITY = UINT64_MAX = 0xFFFFFFFFFFFFFFFF
 
 MIN_TRANSACTION_VALUE = 5
 MAX_TRANSACTION_VALUE = 250
@@ -20,48 +23,53 @@ DEFAULT_TRANSACTION_VALUE = MIN_TRANSACTION_VALUE
 DEFAULT_CHANNEL_CAPACITY = MAX_TRANSACTION_VALUE * 2
 DEFAULT_CHANNEL_BALANCE = MAX_TRANSACTION_VALUE
 
-# These will be changed to TypeVars later.
 
-K = str
-V = int
-
-
-class Graph(MutableMapping[K, dict[K, V]]):
+class Graph(MutableMapping[str, dict[str, int]]):
     """Data type for representing a graph on the network. The underlying
     data structure for storing nodes and channel balances is a hashtable
     mapping node "ids" to channel balances with its peers.
     """
 
-    def __init__(self, nodes: Iterable[K]) -> None:
-        self.nodes = nodes  # type: ignore
-        self.graph = {node: {} for node in self.nodes}  # type: ignore
+    def __init__(self, nodes: Iterable[str]) -> None:
+        self.graph = {node: {} for node in nodes}  # type: ignore
 
     def __repr__(self) -> str:
         fmt = pformat(self.graph)
         return "{}(\n{},\n)".format(type(self).__name__, textwrap.indent(fmt, " " * 4))
 
-    def __getitem__(self, k: K) -> dict[K, V]:
+    def __getitem__(self, k: str) -> dict[str, int]:
         return self.graph[k]
 
-    def __setitem__(self, k: K, v: dict[K, V]) -> None:
+    def __setitem__(self, k: str, v: dict[str, int]) -> None:
         self.graph[k] = v
 
-    def __delitem__(self, k: K) -> None:
+    def __delitem__(self, k: str) -> None:
         del self.graph[k]
 
-    def __iter__(self) -> Iterator[K]:
+    def __iter__(self) -> Iterator[str]:
         return iter(self.graph)
 
     def __len__(self) -> int:
         return len(self.graph)
 
-    @property
-    def nodes(self) -> list[K]:
-        return sorted(self.nodeset)  # type: ignore
+    @classmethod
+    def _from_dict(cls, graph: dict[str, dict[str, int]]) -> Self:
+        """Returns a graph instance as a wrapper over a dict.
 
-    @nodes.setter
-    def nodes(self, nodes: Iterable[K]) -> None:
-        self.nodeset = set(nodes)
+        Note that this class contains a reference to the dict
+        and does not copy its values.
+        """
+        ret = cls(graph.keys())
+        ret.graph = graph
+        return ret
+
+    @property
+    def nodes(self) -> list[str]:
+        return sorted(self)  # type: ignore
+
+    # @nodes.setter
+    # def nodes(self, nodes: Iterable[str]) -> None:
+    #     self = set(nodes)
 
     def reset(self) -> None:
         """Utility function for resetting graph values and channel data."""
@@ -69,13 +77,13 @@ class Graph(MutableMapping[K, dict[K, V]]):
 
     def open_channel(
         self,
-        u: K,
-        v: K,
-        x: V = DEFAULT_CHANNEL_BALANCE,
-        y: V = DEFAULT_CHANNEL_BALANCE,
+        u: str,
+        v: str,
+        x: int = DEFAULT_CHANNEL_BALANCE,
+        y: int = DEFAULT_CHANNEL_BALANCE,
     ) -> None:
         """Opens a channel between nodes `u` and `v`, where `u -> v = x` and `v -> u = y`."""
-        if u not in self.nodeset or v not in self.nodeset:
+        if u not in self or v not in self:
             raise ValueError("Node passed as parameter does not exist.")
         if u == v:
             raise ValueError("Node cannot open channel with itself.")
@@ -87,7 +95,7 @@ class Graph(MutableMapping[K, dict[K, V]]):
         self[u][v] = x
         self[v][u] = y
 
-    def close_channel(self, u: K, v: K) -> None:
+    def close_channel(self, u: str, v: str) -> None:
         """Closes a channel between nodes `u` and `v`. Channel is deleted from graph."""
         if u not in self or v not in self:
             raise ValueError("Node passed as parameter does not exist.")
@@ -97,8 +105,8 @@ class Graph(MutableMapping[K, dict[K, V]]):
         del self[u][v]
         del self[v][u]
 
-    def transfer(self, u: K, v: K, amount: V) -> None:
-        """Transfers an amount `amount` from `u` to `v` through a single channel (u, v)."""
+    def transfer(self, u: str, v: str, amount=DEFAULT_TRANSACTION_VALUE) -> None:
+        """Transfers an amount `amount` from `u` to `v` through a single channel `(u, v)`."""
         if u not in self or v not in self:
             raise ValueError("Node passed as parameter does not exist.")
         if amount < 0:
@@ -111,7 +119,7 @@ class Graph(MutableMapping[K, dict[K, V]]):
         self[u][v] -= amount
         self[v][u] += amount
 
-    def edgecost(self, u: K, v: K) -> V:
+    def edgecost(self, u: str, v: str) -> int:
         """Returns the cost/weight of an edge (u, v) on a graph."""
         try:
             self[u][v]
@@ -119,7 +127,7 @@ class Graph(MutableMapping[K, dict[K, V]]):
             return INFINITY
         return 1
 
-    def dijkstra(self, src: K, dst: K) -> tuple[deque[K], V]:
+    def dijkstra(self, src: str, dst: str) -> tuple[deque[str], int]:
         """Dijkstra's shortest path algorithm for finding the shortest
         path between any two given vertices or nodes on a graph.
 
@@ -127,8 +135,8 @@ class Graph(MutableMapping[K, dict[K, V]]):
             - https://github.com/siddsp02/Dijkstras-Algorithm/blob/main/dijkstra.py
             - https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm
         """
-        dist: dict[K, V] = dict.fromkeys(self, INFINITY)
-        prev: dict[K, V] = dict.fromkeys(self)  # type: ignore
+        dist = dict.fromkeys(self, INFINITY)
+        prev = dict.fromkeys(self)
         dist[src] = 0
         unmarked = set(self)
         while unmarked:
@@ -138,18 +146,18 @@ class Graph(MutableMapping[K, dict[K, V]]):
                 break
             neighbours = self[u].keys()
             for v in neighbours & unmarked:
-                alt = dist[u] + self.edgecost(u, v)
+                alt = dist[u] + self.edgecost(u, v)  # type: ignore
                 if alt < dist[v]:
                     dist[v] = alt
                     prev[v] = u  # type: ignore
-        path = deque[K]()
+        path = deque[str]()
         pred = dst
         while pred is not None:
-            path.appendleft(pred)  # type: ignore
+            path.appendleft(pred)
             pred = prev.get(pred)  # type: ignore
         return path, dist[dst]
 
-    def send(self, src: K, dst: K, amount: V = DEFAULT_TRANSACTION_VALUE) -> TxData:
+    def send(self, src: str, dst: str, amount=DEFAULT_TRANSACTION_VALUE) -> TxData:
         """Sends an amount `amount` from `src` to `dst` based
         on the shortest path between the two if one exists.
         """
@@ -162,17 +170,17 @@ class Graph(MutableMapping[K, dict[K, V]]):
             self.transfer(u, v, amount)
         return TxData(path, src, dst, amount, len(path) - 1, TxStatus.SUCCESS)
 
-    def get_balance(self, node: K) -> V:
+    def get_balance(self, node: str) -> int:
         """Returns the outgoing balance of a node on a graph."""
-        return sum(self[node].values())  # type: ignore
+        return sum(self[node].values())
 
-    def get_node(self, node: K) -> Node:
+    def get_node(self, node: str) -> Node:
         """Returns a node instance that belongs to the graph."""
-        if node not in self.nodeset:
+        if node not in self:
             raise KeyError("Node does not exist in graph.")
         return Node(node, self)
 
-    def max_sendable(self, src: K, dst: K) -> V:
+    def max_sendable(self, src: str, dst: str) -> int:
         """Returns the maximum amount that can be sent from the source
         to the destination node (assuming the shortest path)."""
         path, _ = self.dijkstra(src, dst)
@@ -190,7 +198,7 @@ class Node:
     graph: Graph = field(repr=False)
 
     def __post_init__(self) -> None:
-        if self.name not in self.graph.nodeset:
+        if self.name not in self.graph:
             raise ValueError("Node does not exist on graph.")
 
     def __str__(self) -> str:
@@ -204,7 +212,7 @@ class Node:
     def channels(self) -> dict[str, int]:
         return self.graph[self.name]
 
-    def send(self, node: Self | str, amount: V = DEFAULT_TRANSACTION_VALUE) -> TxData:
+    def send(self, node: Self | str, amount=DEFAULT_TRANSACTION_VALUE) -> TxData:
         return self.graph.send(self.name, str(node), amount)
 
     def open_channel(
@@ -220,8 +228,7 @@ class Node:
 
 
 def main() -> None:
-    graph = Graph(["Alice", "Bob", "Carol", "David", "Ella", "Frank"])
-    graph.update(
+    graph = Graph._from_dict(
         {
             "Alice": {"Bob": 4, "Carol": 3},
             "Bob": {"Alice": 1, "Carol": 4, "David": 3},
@@ -231,9 +238,6 @@ def main() -> None:
             "Frank": {"David": 3, "Ella": 3},
         }
     )
-    print(graph)
-    alice = graph.get_node("Alice")
-    print(alice.send("Frank", 1))
     print(graph)
 
 
