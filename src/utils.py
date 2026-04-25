8f95a236e618ca23e971a0b4cd9692379466124c
Sidd <sidd.s.pai@gmail.com>
2026-04-25 15:16:07 -0600

    Removed unused functions.

--- src/utils.py
+++ src/utils.py
@@ -1,12 +1,8 @@
-import doctest
-import math
-import random
 import textwrap
 from collections import deque
-from decimal import Decimal
 from enum import Enum, auto
-from itertools import chain, product, starmap
-from typing import Iterable, Iterator, NamedTuple
+from itertools import starmap
+from typing import NamedTuple
 
 
 class TxStatus(Enum):
@@ -29,64 +25,3 @@ class TxData[T](NamedTuple):
             type(self).__name__,
             textwrap.indent(",\n".join(starmap("{}={!r}".format, pairs)), " " * 4),
         )
-
-
-def rand(precision: int = 2) -> Decimal:
-    """Returns a random decimal value in the interval [0, 1).
-
-    Default precision is 2.
-    """
-    val = random.random()
-    return Decimal(f"{val:.{precision}f}")
-
-
-def rand_between(lower: Decimal, upper: Decimal, precision: int = 2) -> Decimal:
-    """Returns a random Decimal value between an upper and lower bound."""
-    result = random.uniform(float(lower), float(upper))
-    return round(Decimal(result), precision)
-
-
-def generate_node_names(chars: str, maxlen: int = 2) -> Iterator[str]:
-    """Utility function for generating node names for a large graph.
-    Make sure that the string is all unique characters for this
-    function to work properly.
-
-    This gives out all possible substrings of length 1 to `maxlen`
-    from characters of `chars`.
-
-    Examples:
-    >>> list(generate_node_names("ab", maxlen=2))
-    ['a', 'b', 'aa', 'ab', 'ba', 'bb']
-    >>> list(generate_node_names("ab", maxlen=3))
-    ['a', 'b', 'aa', 'ab', 'ba', 'bb', 'aaa', 'aab', 'aba', 'abb', 'baa', 'bab', 'bba', 'bbb']
-    """
-    return chain.from_iterable(
-        map("".join, product(chars, repeat=i)) for i in range(1, maxlen + 1)
-    )
-
-
-# The following code was taken from the link below (see end).
-# https://docs.python.org/3/library/itertools.html
-
-
-def nth_combination[T](iterable: Iterable[T], r: int, index: int) -> tuple[T, ...]:
-    "Equivalent to list(combinations(iterable, r))[index]"
-    pool = tuple(iterable)
-    n = len(pool)
-    c = math.comb(n, r)
-    if index < 0:
-        index += c
-    if index < 0 or index >= c:
-        raise IndexError()
-    result = []
-    while r:
-        c, n, r = c * r // n, n - 1, r - 1
-        while index >= c:
-            index -= c
-            c, n = c * (n - r) // n, n - 1
-        result.append(pool[-1 - n])
-    return tuple(result)
-
-
-if __name__ == "__main__":
-    doctest.testmod()
